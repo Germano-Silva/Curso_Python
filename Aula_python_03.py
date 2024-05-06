05805b5ca8522c24d99c6b07cd1e92b5ac8bd93b
Germano-Silva <developer.germano.silva@gmail.com>
2024-05-06 20:36:35 -0300

    Atualização 03

---
+++ Aula_python_03.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Germano Rodrigues')
+print(1, 'Germano "Rodrigues"')
+
+# Aspas duplas
+print("Germano Rodrigues")
+print(2, "Germano 'Rodrigues'")
+
+# Escape
+print("Germano \"Rodrigues\"")
+
+# r
+print(r"Germano \"Rodrigues\"")

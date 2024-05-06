30d0c187a1ec40e2270d6ab8dec539cd0d691350
Germano-Silva <developer.germano.silva@gmail.com>
2024-05-06 20:52:44 -0300

    Atualizado para pasta de aulas

--- Aula_python_01.py
+++
@@ -1,5 +0,0 @@
-#O interpretador ignora e da de escrever um comentario nessa linha.
-print(123)
-"""
-Não é um comentario, é chamado de DocStrings e é lido na execução do codigo.
-"""
--- Aula_python_10.py
+++
@@ -1,7 +0,0 @@
-concatenacao = 'a'+'b'+'c'
-print(concatenacao)
-
-a_dez_vezes = 'a'*10
-trez_vezes_germano = 3* 'Germano'
-print(a_dez_vezes)
-print(trez_vezes_germano)

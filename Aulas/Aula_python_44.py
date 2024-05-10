455dbf48e8cb3c98e83dcee1f3164b5a7cbdcadf
Germano-Silva <developer.germano.silva@gmail.com>
2024-05-10 20:40:14 -0300

    Atualização 44

--- Aulas/Aula_python_44.py
+++ Aulas/Aula_python_44.py
@@ -2,6 +2,9 @@
 For + Range
 range -> range(start, stop, step)
 """
+# Este trecho de código está criando um intervalo de números começando de 0 até (mas não incluindo) 100 com
+# um tamanho de passo de 8. A chamada de função `range(0, 100, 8)` gera números em incrementos de 8 dentro
+# o intervalo especificado.
 numeros = range(0, 100, 8)
 
 for numero in numeros:

25e2df66d5d657ff7dc7132d1e12461566d31ed4
Germano-Silva <developer.germano.silva@gmail.com>
2024-05-10 20:40:45 -0300

    Atualização 46

--- Aulas/Aula_python_46.py
+++ Aulas/Aula_python_46.py
@@ -1,3 +1,5 @@
+# Este trecho de código Python está usando uma estrutura de loop aninhado com uma combinação de `for` e `if`
+# declarações. Aqui está um resumo do que ele faz:
 for i in range(10):
     if i == 2:
         print('i é 2, pulando...')

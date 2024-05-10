1bbf7df67f22acdab9da95ffe971afacb9655c2e
Germano-Silva <developer.germano.silva@gmail.com>
2024-05-10 20:40:00 -0300

    Atualização 43

--- Aulas/Aula_python_43.py
+++ Aulas/Aula_python_43.py
@@ -9,6 +9,9 @@
 
 # print(repeticoes)
 # print('Aquele laço acima pode ter repetições infinitas')
+# Este código está iterando sobre cada caractere da string `texto`, que é 'Python'. Para cada
+# caractere, é adicionar um asterisco (*) antes do caractere e anexá-lo ao `novo_texto`
+# variável.
 texto = 'Python'
 
 novo_texto = ''

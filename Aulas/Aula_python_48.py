4ffa573eb8570b2312300af9729d86603d2fdbb2
Germano-Silva <developer.germano.silva@gmail.com>
2024-05-14 20:07:40 -0300

    Atualização 48

--- Aulas/Aula_python_48.py
+++ Aulas/Aula_python_48.py
@@ -5,3 +5,6 @@ Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, +
 """
+#        +01234
+#        -54321
+string = 'ABCDE'  # 5 caracteres (len)
